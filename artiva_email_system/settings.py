030d1d07f366b4cc0b41626aa16a622d5ecb34ce
Manish Kumar <manish.kumar@aujas.com>
2019-10-27 16:03:25 +0530

    updated settings.py

--- artiva_email_system/settings.py
+++ artiva_email_system/settings.py
@@ -129,7 +129,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'artivatic.test1@gmail.com'
-EMAIL_HOST_PASSWORD = 'xxxxxxx'
+EMAIL_HOST_PASSWORD = 'xxxxxx'
 
 
 
